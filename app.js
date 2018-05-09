c1e4a93dac5a1ca25bc7fef278eec7a057babb56
Lance Strait <lance@Lances-MacBook-Pro.local>
2018-05-09 14:20:34 -0500

    added DB connection

--- app.js
+++ app.js
@@ -3,13 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var scraper = require('./libs/scraper');
+var dbConfig = require('./config/dev');
 
 var app = express();
+
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://' + dbConfig.user +':' + dbConfig.pass + '@ds119660.mlab.com:19660/gradcafe_results'
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
