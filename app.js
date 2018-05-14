06744a42f8d6282fb359e2a78c051a2f28011232
Lance Strait <lancestrait10@hotmail.com>
2018-05-14 17:41:58 -0500

    prepping for heroku deployment

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb://' + dbConfig.user +':' + dbConfig.pass + '@ds119660.mlab.com:19660/gradcafe_results'
+var mongoDB = process.env.MONGODB_URI || 'mongodb://' + dbConfig.user +':' + dbConfig.pass + '@ds119660.mlab.com:19660/gradcafe_results'
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
