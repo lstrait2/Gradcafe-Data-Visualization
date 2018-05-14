d4c8914369429d11a49c4c2a8c0b4ca38b557bca
Lance Strait <lancestrait10@hotmail.com>
2018-05-14 18:23:49 -0500

    set up react homepage

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,42 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  // Initialize state
+  state = { institutions: [] }
+
+  // Fetch decisions after first mount
+  componentDidMount() {
+    this.getInstitutions();
+  }
+
+  getInstitutions = () => {
+    // Get the passwords and store them in state
+    fetch('/decisions/institutions/')
+      .then(res => res.json())
+      .then(passwords => this.setState({ passwords }));
+  }
+
   render() {
+    const { institutions } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="institutions">
+              {/*
+                Generally it's bad to use "index" as a key.
+                It's ok for this example because there will always
+                be the same number of passwords, and they never
+                change positions in the array.
+              */}
+              {institutions.map((institution, index) =>
+                <li key={index}>
+                  {institution}
+                </li>
+              )}
+            </ul>
       </div>
     );
   }
